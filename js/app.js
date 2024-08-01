b506e676fbc20a942135b78a1ddd1cdb2e377f5e
Nathalia Harumi Asai <145270943+NathaliaAsai@users.noreply.github.com>
2024-07-31 21:00:49 -0300

    caracter corrigido

--- js/app.js
+++ js/app.js
@@ -1,22 +1,22 @@
 // inicializar o status dos jogos
 function inicializarStatus() {
-if(!localStorage.getItem('statusJogos')) { //!= operador lÃ³gico de NEGAÃ‡ÃƒO, if serÃ¡ executado se false
-  //localStorage Ã© um armazenamento persistente, enquanto let/const Ã© um armazenamento temporario (dados sÃ£o perdidos ao carregar a pÃ¡gina)
-  const statusInicial = 'DisponÃ­vel, DisponÃ­vel, DisponÃ­vel';
-    //usar let qnd a variavel possa ser reatribuido ao longo do cÃ³digo, const qnd a variavel nao for mudar
+if(!localStorage.getItem('statusJogos')) { //!= operador lógico de NEGAÇÃO, if será executado se false
+  //localStorage é um armazenamento persistente, enquanto let/const é um armazenamento temporario (dados são perdidos ao carregar a página)
+  const statusInicial = 'Disponí­vel, Disponí­vel, Disponível';
+    //usar let qnd a variavel possa ser reatribuido ao longo do código, const qnd a variavel nao for mudar
     localStorage.setItem('statusJogos', statusInicial);
   }
   atualizarInterface(); 
 }
 
-//FuncÃ£o para atualizar a interface dos jogos
+//Funcão para atualizar a interface dos jogos
 function atualizarInterface() {
   let statusJogos = localStorage.getItem('statusJogos'). split(','); // para transformar a string (texto) armazenada em localStorage em um array (lista/ colecoes)
   statusJogos.forEach((status, index) => {
-      let gameElement = document.getElementById('game-' + (index + 1)); //seleciona um elemento html especÃ­fico com base no identificado (Ã­d)
+      let gameElement = document.getElementById('game-' + (index + 1)); //seleciona um elemento html específico com base no identificado (id)
       let button = gameElement.querySelector('.dashboard__item__button');
 
-      if (status === 'DisponÃ­vel') { // == igualdade solta (pode fazer conversao e comparar 5 == `5`), === igualdade estrita (5 diferente de `5`)
+      if (status === 'Disponível') { // == igualdade solta (pode fazer conversao e comparar 5 == `5`), === igualdade estrita (5 diferente de `5`)
           gameElement.querySelector('.dashboard__item__img').classList.remove('dashboard__item__img--rented');
           button.textContent = 'Alugar';
           button.classList.remove('dashboard__item__button--return');
@@ -28,17 +28,17 @@ function atualizarInterface() {
   });
 }
 
-// FunÃ§Ã£o para alterar o status de um jogo
+// Função para alterar o status de um jogo
 function alterarStatus(gameId) {
   let statusJogos = localStorage.getItem('statusJogos').split(',');
   let statusAtual = statusJogos[gameId - 1];
 
-  if (statusAtual === 'DisponÃ­vel') {
+  if (statusAtual === 'Disponível') {
       statusJogos[gameId - 1] = 'Alugado';
-      alert('VocÃª alugou o jogo!');
+      alert('Você alugou o jogo!');
   } else {
       statusJogos[gameId - 1] = 'DisponÃ­vel';
-      alert('VocÃª devolveu o jogo!');
+      alert('Você devolveu o jogo!');
   }
 
   localStorage.setItem('statusJogos', statusJogos.join(',')); // join converte arrat em string
